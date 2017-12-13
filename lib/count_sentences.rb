ab3ce9ad95c19501c58804af911098ec85f9bbb5
Neil <neilmarkoliverryan@gmail.com>
2017-12-12 22:37:27 -0800

    completed code challenge

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	end_with?(".")
   end
 
   def question?
-
+  	end_with?("?")
   end
 
   def exclamation?
-
+  	end_with?("!")
   end
 
   def count_sentences
-
+  	array = split(/[.?!]/)
+  	array.delete_if {|e| e == "" || e == " "}
+  	array.count
   end
+
 end
